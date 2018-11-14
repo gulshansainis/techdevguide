1a7a9dec42272c7d2858c6d7de83431537be2865
gulshansainis <gulshan.saini@gmail.com>
2018-11-14 22:09:37 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Tech Dev Guide', // Required
+  author: 'Gulshan Saini', // Required
+  description: 'Full-stack Web Developer',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://gulshansainis.com',
+    github: 'https://github.com/gulshansainis',
+    twitter: 'https://twitter.com/gulshansainis',
+    linkedin: 'https://linkedin.com/in/gulshansainis',
+  },
+  pathPrefix: '/',
+  siteUrl: 'https://techdevguide.com',
+};
