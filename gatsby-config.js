e442745d88b4c5f7a8ac0757e9e0aa452b7c0dc0
gulshansainis <gulshan.saini@gmail.com>
2018-12-24 20:40:28 +0530

    fixed favicon issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Techdevguide',
     author: 'Gulshan Saini',
-    description:
-      'Personal blog by Gulshan Saini. For developer by developer.',
+    description: 'Personal blog by Gulshan Saini. For developer by developer.',
     siteUrl: 'https://techdevguide.com',
     social: {
       twitter: '@gulshansainis',
@@ -64,6 +63,48 @@ module.exports = {
         theme_color: `#423884`,
         display: `minimal-ui`,
         icon: `src/assets/icon.png`,
+        icons: [
+          {
+            src: 'favicons/icon-48x48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: 'favicons/icon-72x72.png',
+            sizes: '72x72',
+            type: 'image/png',
+          },
+          {
+            src: 'favicons/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+          },
+          {
+            src: 'favicons/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+          },
+          {
+            src: 'favicons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicons/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: 'favicons/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: 'favicons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
       },
     },
     `gatsby-plugin-react-helmet`,
@@ -73,5 +114,6 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    `gatsby-plugin-offline`
   ],
 }
